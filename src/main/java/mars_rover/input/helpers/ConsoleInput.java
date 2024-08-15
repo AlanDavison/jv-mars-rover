9c6190223fbb3ac916c5de7d58d9fc71c7f43470
Alan Davison <contact@decidedlyhuman.co.uk>
2024-08-15 10:52:03 +0100

    Implement basic repeating ConsoleInput class with validation support.

--- src/main/java/mars_rover/input/helpers/ConsoleInput.java
+++ src/main/java/mars_rover/input/helpers/ConsoleInput.java
@@ -1,4 +1,20 @@
 package mars_rover.input.helpers;
 
+import java.util.Scanner;
+import java.util.function.Predicate;
+
 public class ConsoleInput {
+    public static String getConsoleInput(String printMessage, String invalidMessage, Scanner scanner, Predicate<String> predicate) {
+        String input = "";
+
+        while (!predicate.test(input)) {
+            System.out.print(printMessage);
+            input = scanner.nextLine();
+
+            if (!predicate.test(input))
+                System.out.println(invalidMessage);
+        }
+
+        return input;
+    }
 }
