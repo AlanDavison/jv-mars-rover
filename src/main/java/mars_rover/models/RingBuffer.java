f6b27faf7b88e3c5c0838f3501189c5772f2a7d0
Alan Davison <contact@decidedlyhuman.co.uk>
2024-08-15 15:35:34 +0100

    Implement RingBuffer class.

---
+++ src/main/java/mars_rover/models/RingBuffer.java
@@ -0,0 +1,34 @@
+package mars_rover.models;
+
+import java.util.ArrayList;
+
+public class RingBuffer<E> {
+    ArrayList<E> elements;
+    int currentIndex = 0;
+
+    public RingBuffer() {
+        this.elements = new ArrayList<>();
+    }
+
+    public void add(E element) {
+        this.elements.add(element);
+    }
+
+    public void shiftDown() {
+        if (this.currentIndex == 0)
+            this.currentIndex = this.elements.size() - 1;
+        else
+            this.currentIndex--;
+    }
+
+    public void shiftUp() {
+        if (this.currentIndex == this.elements.size() - 1)
+            this.currentIndex = 0;
+        else
+            this.currentIndex++;
+    }
+
+    public E getCurrentElement() {
+        return this.elements.get(currentIndex);
+    }
+}
