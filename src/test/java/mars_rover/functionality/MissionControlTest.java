8415fecfea7ad92c52b94b0b060d9b2fbe4f61dd
Alan Davison <contact@decidedlyhuman.co.uk>
2024-08-15 17:47:10 +0100

    Add MissionControl tests.

---
+++ src/test/java/mars_rover/functionality/MissionControlTest.java
@@ -0,0 +1,34 @@
+package mars_rover.functionality;
+
+import mars_rover.models.Plateau;
+import mars_rover.models.Position;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MissionControlTest {
+
+    @Test
+    @DisplayName("Test Position validity checking.")
+    void testPositionValidity() {
+        MissionControl control = new MissionControl(new Plateau(10, 10));
+        Rover roverOne = new Rover(new Position(3, 5));
+        Rover roverTwo = new Rover(new Position(0, 0));
+        Rover roverThree = new Rover(new Position(0, 10));
+        Rover roverFour = new Rover(new Position(10, 0));
+        control.addRover(roverOne);
+        control.addRover(roverTwo);
+        control.addRover(roverThree);
+        control.addRover(roverFour);
+
+        assertTrue(control.isPositionFree(new Position(0, 1)));
+        assertTrue(control.isPositionFree(new Position(1, 0)));
+        assertTrue(control.isPositionFree(new Position(10, 10)));
+        assertFalse(control.isPositionFree(new Position(10, 0)));
+        assertFalse(control.isPositionFree(new Position(0, 10)));
+        assertFalse(control.isPositionFree(new Position(3, 5)));
+        assertFalse(control.isPositionFree(new Position(0, 0)));
+    }
+
+}
