62e1cc17ba2c9acb798b92ffc6de7fc59926329b
Alan Davison <contact@decidedlyhuman.co.uk>
2024-08-15 10:32:48 +0100

    Add simple Orientation test.

--- src/test/java/mars_rover/models/OrientationTest.java
+++ src/test/java/mars_rover/models/OrientationTest.java
@@ -1,6 +1,8 @@
 package mars_rover.models;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,6 +18,24 @@ class OrientationTest {
         orientationEast = Orientation.EAST;
         orientationSouth = Orientation.SOUTH;
         orientationWest = Orientation.WEST;
+    }
+
+    @Test
+    @DisplayName("Test the associated movement direction with each enum value.")
+    public void testMovementDirections() {
+        Vector2 expectedNorthMovement = new Vector2(0, 1);
+        Vector2 expectedEastMovement = new Vector2(1, 0);
+        Vector2 expectedSouthMovement = new Vector2(0, -1);
+        Vector2 expectedWestMovement = new Vector2(-1, 0);
+
+        assertNotEquals(expectedWestMovement, expectedNorthMovement);
+        assertNotEquals(expectedEastMovement, expectedNorthMovement);
+        assertAll(
+                () -> assertEquals(expectedNorthMovement, expectedNorthMovement),
+                () -> assertEquals(expectedEastMovement, expectedEastMovement),
+                () -> assertEquals(expectedSouthMovement, expectedSouthMovement),
+                () -> assertEquals(expectedWestMovement, expectedWestMovement)
+        );
     }
 
 }
