2374c6dd95106c4e94e7701698689081f1aed3ce
Alan Davison <contact@decidedlyhuman.co.uk>
2024-08-15 15:37:43 +0100

    Update Rover class to store Orientation as a RingBuffer.

--- src/main/java/mars_rover/functionality/Rover.java
+++ src/main/java/mars_rover/functionality/Rover.java
@@ -3,14 +3,19 @@ package mars_rover.functionality;
 import mars_rover.models.Instruction;
 import mars_rover.models.Orientation;
 import mars_rover.models.Position;
+import mars_rover.models.RingBuffer;
 
 public class Rover {
     private Position position;
-    private Orientation orientation;
+    private RingBuffer<Orientation> orientation;
 
     public Rover(Position position) {
         this.position = position;
-        this.orientation = Orientation.NORTH;
+        this.orientation = new RingBuffer<>();
+        this.orientation.add(Orientation.NORTH);
+        this.orientation.add(Orientation.SOUTH);
+        this.orientation.add(Orientation.EAST);
+        this.orientation.add(Orientation.WEST);
     }
 
     public Position getPosition() {
@@ -18,7 +23,7 @@ public class Rover {
     }
 
     public Orientation getOrientation() {
-        return this.orientation;
+        return this.orientation.getCurrentElement();
     }
 
     public void receiveInstruction(Instruction instruction) {
