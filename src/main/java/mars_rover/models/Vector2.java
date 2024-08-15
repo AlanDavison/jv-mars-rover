4dccddd5e599580a8502a6569c0267e3bf2861e0
Alan Davison <contact@decidedlyhuman.co.uk>
2024-08-15 10:42:23 +0100

    Make Vector2 class pass equality tests.

--- src/main/java/mars_rover/models/Vector2.java
+++ src/main/java/mars_rover/models/Vector2.java
@@ -4,22 +4,36 @@ public class Vector2 {
     private int x, y;
 
     public Vector2(int x, int y) {
-
+        this.x = x;
+        this.y = y;
     }
 
     public int getX() {
-        return -1;
+        return this.x;
     }
 
     public int getY() {
-        return -1;
+        return this.y;
     }
 
     public void setX(int x) {
-
+        this.x = x;
     }
 
     public void setY(int y) {
+        this.y = y;
+    }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null)
+            return false;
+        if (!(obj instanceof Vector2))
+            return false;
+        if (obj == this)
+            return true;
+
+        return ((Vector2) obj).getX() == this.getX() &&
+                ((Vector2) obj).getY() == this.getY();
     }
 }
