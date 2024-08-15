6501f0d7ea19de6518900ef883aebe97d413ef42
Alan Davison <contact@decidedlyhuman.co.uk>
2024-08-15 15:08:45 +0100

    Add Plateau skeleton.

--- src/main/java/mars_rover/models/Plateau.java
+++ src/main/java/mars_rover/models/Plateau.java
@@ -1,4 +1,18 @@
 package mars_rover.models;
 
 public class Plateau {
+    private final int width, height;
+
+    public Plateau(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return this.width;
+    }
+
+    public int getHeight() {
+        return this.height;
+    }
 }
