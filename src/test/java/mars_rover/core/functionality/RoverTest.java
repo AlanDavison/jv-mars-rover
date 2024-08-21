35a2958b2ea65c0a43b65d29d3ed7a42a38e425b
Alan Davison <contact@decidedlyhuman.co.uk>
2024-08-21 14:02:01 +0100

    Beef up Rover tests significantly.

--- src/test/java/mars_rover/core/functionality/RoverTest.java
+++ src/test/java/mars_rover/core/functionality/RoverTest.java
@@ -1,10 +1,15 @@
 package mars_rover.core.functionality;
 
+import mars_rover.core.helpers.InstructionParser;
 import mars_rover.core.models.MovementInstruction;
 import mars_rover.core.models.Orientation;
+import mars_rover.core.models.Plateau;
 import mars_rover.core.models.Position;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,14 +20,63 @@ class RoverTest {
     void testReceiveRotationInstructions() {
         Rover rover = new Rover(new Position(4, 6), 0);
 
-        rover.receiveInstruction(MovementInstruction.TurnLeft);
+        rover.receiveInstruction(MovementInstruction.TurnRight);
+        assertEquals(Orientation.EAST, rover.getOrientation());
+
+        rover.receiveInstruction(MovementInstruction.TurnRight);
+        assertEquals(Orientation.SOUTH, rover.getOrientation());
+
+        rover.receiveInstruction(MovementInstruction.TurnRight);
         assertEquals(Orientation.WEST, rover.getOrientation());
 
         rover.receiveInstruction(MovementInstruction.TurnRight);
         assertEquals(Orientation.NORTH, rover.getOrientation());
 
-        rover.receiveInstruction(MovementInstruction.TurnRight);
+        rover.receiveInstruction(MovementInstruction.TurnLeft);
+        assertEquals(Orientation.WEST, rover.getOrientation());
+
+        rover.receiveInstruction(MovementInstruction.TurnLeft);
+        assertEquals(Orientation.SOUTH, rover.getOrientation());
+
+        rover.receiveInstruction(MovementInstruction.TurnLeft);
         assertEquals(Orientation.EAST, rover.getOrientation());
+
+        rover.receiveInstruction(MovementInstruction.TurnLeft);
+        assertEquals(Orientation.NORTH, rover.getOrientation());
+
+        rover = new Rover(new Position(4, 6), 0);
+        MissionControl control = new MissionControl(new Plateau(10, 10));
+
+        control.addRover(rover);
+        String moveCommand = "RRRR";
+
+        assertEquals(Orientation.NORTH, rover.getOrientation());
+
+        Optional<LinkedList<MovementInstruction>> instructions = InstructionParser.parseInstructions(moveCommand);
+
+        for (var instruction: instructions.get()) {
+            if (!control.getCurrentRover().isPresent())
+                continue;
+
+            control.receiveInstruction(control.getCurrentRover().get(), instruction);
+        }
+
+        assertEquals(Orientation.NORTH, rover.getOrientation());
+
+        moveCommand = "LLLL";
+
+        assertEquals(Orientation.NORTH, rover.getOrientation());
+
+        instructions = InstructionParser.parseInstructions(moveCommand);
+
+        for (var instruction: instructions.get()) {
+            if (!control.getCurrentRover().isPresent())
+                continue;
+
+            control.receiveInstruction(control.getCurrentRover().get(), instruction);
+        }
+
+        assertEquals(Orientation.NORTH, rover.getOrientation());
     }
 
     @Test
@@ -71,5 +125,29 @@ class RoverTest {
 
         rover.receiveInstruction(MovementInstruction.MoveForward);
         assertEquals(new Position(4, 10), rover.getPosition());
+    }
+
+    @Test
+    @DisplayName("Test receiving instructions via MissionControl.")
+    void testReceiveInstructionsViaMissionControl() {
+        MissionControl control = new MissionControl(new Plateau(10, 10));
+        Rover rover = new Rover(new Position(5, 5), 0);
+        control.addRover(rover);
+        String moveCommand = "MLMMRMRMRMLMRMRR";
+
+        assertEquals(new Position(5, 5), rover.getPosition());
+        assertEquals(Orientation.NORTH, rover.getOrientation());
+
+        Optional<LinkedList<MovementInstruction>> instructions = InstructionParser.parseInstructions(moveCommand);
+
+        for (var instruction: instructions.get()) {
+            if (!control.getCurrentRover().isPresent())
+                continue;
+
+            control.receiveInstruction(control.getCurrentRover().get(), instruction);
+        }
+
+        assertEquals(new Position(5, 5), rover.getPosition());
+        assertEquals(Orientation.NORTH, rover.getOrientation());
     }
 }
