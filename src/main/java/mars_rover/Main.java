1fc0d33f126b9d556e65de2398b2e7bbf0031e28
Alan Davison <contact@decidedlyhuman.co.uk>
2024-08-21 15:00:57 +0100

    Refactor entry point to Main.

--- src/main/java/mars_rover/Main.java
+++ src/main/java/mars_rover/Main.java
@@ -2,10 +2,9 @@ package mars_rover;
 
 import mars_rover.console.ConsoleRunner;
 
-public class MarsRover {
+public class Main {
     public static void main(String[] args) {
         ConsoleRunner runner = new ConsoleRunner();
-
         runner.run();
     }
 }
